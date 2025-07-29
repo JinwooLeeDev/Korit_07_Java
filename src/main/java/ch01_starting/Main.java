fb85cdad240014a5b37a6e1aaf6323e8b61f3481
JinwooLeeDev <jinwoo_dev@naver.com>
2025-07-29 16:06:23 +0900

    feat: 20250729 lesson

---
+++ src/main/java/ch01_starting/Main.java
@@ -0,0 +1,11 @@
+package ch01_starting;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+        System.out.println("한글");
+        System.out.println("123");
+//        System.out.println("");
+        // ch02_variable 패키지 생성 -> Main 클래스 생성
+    }
+}
